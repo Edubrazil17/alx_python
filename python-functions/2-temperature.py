2f9153fb36b7b5c950569bebf3f84019c3eefe04
EZE <chinedu.eze009@gmail.com>
2023-07-26 03:59:45 -0700

    This program conver (fehrenhelt) to (celsus)

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -3,8 +3,3 @@
 def convert_to_celsus(fahrenhelt):
     celsus = (fahrenhelt - 32)*5/9
     return celsus
-
-print(convert_to_celsus(78))
-print(convert_to_celsus(100))
-print(convert_to_celsus(-40))
-print(convert_to_celsus(-459.67))
