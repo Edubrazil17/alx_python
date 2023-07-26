6a366a7ce1b4bec1a13c3407169dc32bba294c34
EZE <chinedu.eze009@gmail.com>
2023-07-25 21:15:59 -0700

    sum number

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-def sum (a, b):
-    return a + b
-   print(sum(1, 2))
-   print(sum(100, -2))
-   print(sum(100, 2))
+def sum(a, b):
+    return a+b
+print(sum(1, 2))
+print(sum(100, 2))
+print(sum(-100, -2))
+print(sum(98, 8))
