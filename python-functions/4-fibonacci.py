a199b76e1debd565122e9c3c09763c9fbb409bab
EZE <chinedu.eze009@gmail.com>
2023-07-26 04:10:52 -0700

    This program print the fibonancci sequence

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -10,16 +10,3 @@ def fibonacci_sequence(n):
         fibonacci_sequence.append(next_number)
 
     return fibonacci_sequence[:n]
-
-#call the function to print the fib. sequence of given numbr
-n = 8
-print(fibonacci_sequence(8))
-
-n = 10
-print(fibonacci_sequence(10))
-
-n = 50
-print(fibonacci_sequence(50))
-
-n = 1
-print(fibonacci_sequence(1))
