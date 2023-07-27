cd4efe182bc6b94e7adac5bddf82d67140f6132e
EZE <chinedu.eze009@gmail.com>
2023-07-26 22:05:44 -0700

    This program import a add function

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#import function to add two number together
+
+from add_0 import add
+
+a = 1
+b = 2
+result = add(a, b)
+#print the result with string format
+
+print("{} + {} = {}".format(a, b, result))
