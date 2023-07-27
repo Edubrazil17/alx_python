471a9e6a52d25c6049ee24990f26e9c9e533a53b
EZE <chinedu.eze009@gmail.com>
2023-07-26 22:52:16 -0700

    This program print true or false to a prime number

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -4,12 +4,12 @@ def is_prime(number):
 
     #This is the first condition that checks if the number is less than 2. Since prime numbers are greater than 1
     if number < 2:
-        return false
+        return False
 
 #This is a loop that iterates from 2 to the square root of the input number (inclusive)
     for i in range (2, int(number **0.5) + 1):
 
         if number % i == 0:
-            return false
+            return False
 
-        return true
+        return True
