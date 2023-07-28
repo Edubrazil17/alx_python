464132a5b6262c47f9d5cc6542027ffb8ea8e948
EZE <chinedu.eze009@gmail.com>
2023-07-27 21:48:03 -0700

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 #import function to add two number together
-import add_0
+from add_0 import add
 
 a = 1
 b = 2
-result = add_0.add(a, b)
+result = add(a, b)
 #print the result with string format
 
 print("{} + {} = {}".format(a, b, result))
+
+if __name=="__main__":
+   
