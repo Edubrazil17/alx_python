bc4ffd8059c53d5f6fd33e5f63459518b82e442a
EZE <chinedu.eze009@gmail.com>
2023-07-27 21:43:15 -0700

    A program that prints the number of and the list of its arguments.

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+def print_argument():
+    num_arg = len(sys.argv)-1
+
+    if num_arg == 0:
+        print("0 arguments:")
+    elif num_arg == 1:
+        print("1 arguments:")
+    else:
+        print(f"{num_arg} arguments:")
+
+    if num_arg > 0:
+        for i in range(1, num_arg + 1 ):
+            print(f"{i}:{sys.argv[i]}")
+
+            
+if __name__ == "__main__":
+   print_argument()
