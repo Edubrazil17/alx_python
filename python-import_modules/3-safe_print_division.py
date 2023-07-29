082ebfb4383b35d16995b0d1d497749979d056e0
EZE <chinedu.eze009@gmail.com>
2023-07-29 09:05:10 -0700

    A function that divide 2 integer and print it result

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -12,16 +12,3 @@ def safe_print_division(a, b):
     finally:
         print("inside result = {}".format(result))
         return result
-        
-
-
-a = 6
-b= 3
-result = safe_print_division(a, b)
-
-print("{:d}/{:d} = {}".format(a, b,result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d}/{:d} = {}".format(a, b, result))
