2e43258e598dc061c25dd3555aafbd3e4890f664
EZE <chinedu.eze009@gmail.com>
2023-07-29 08:51:30 -0700

    import modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -10,11 +10,11 @@ def print_argument():
     elif num_arg == 1:
         print("1 argument:")
     else:
-        print(f"{num_arg}" " arguments:")
+        print(f"{num_arg} arguments:")
 
     if num_arg > 0:
         for i in range(1, num_arg + 1 ):
-            print(f"{i}:{sys.argv[i]}")
+            print(f"{i}:" "{sys.argv[i]}")
 
             
 if __name__ == "__main__":
