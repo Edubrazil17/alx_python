4bf60f46c7bbaa178318f61cb7b2f0a7d65796a9
EZE <chinedu.eze009@gmail.com>
2023-08-01 21:37:30 -0700

    A function that return a key with the bigest integer value

--- python-more_data_structures/3_best_score.py
+++ python-more_data_structures/3_best_score.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None or is not a_dictionary
-    return None
+    if a_dictionary is None or not a_dictionary:
+        return None
 
-# initialize varibles that track the highest value
+# initialize varibles to track of the highest value and corresponding key
 
-best_score = float('-inf')# start from negative inifinity
-best_key = None
+    best_score = float('-inf')# start with negative inifinity
+    best_key = None
 
 # iterate through the a_dictionary for the best_score and corresponding key
 
-for key, value in a_dictionary.items():
+    for key, value in a_dictionary.items():
 
-# check if best_score is greater than current value 
-    if best_score > value:
+# check if value is greater than current value 
+        if value > best_score:
 
 #if it does, update best_score and it corresponding key 
-        best_score = value
-        best_key = key
+           best_score = value
+           best_key = key
 
-return best_key
+    return best_key
