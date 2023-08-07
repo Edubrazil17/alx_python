4ed49bbded8169be971952f062bd14d3dd13cb08
EZE <chinedu.eze009@gmail.com>
2023-08-07 08:30:33 -0700

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Square Module.
+
+This module defines a Square class that represents a square with a private
+instance attribute: size.
+
+Example:
+    Create a square instance with a size of 3:
+    >>> my_square = Square(3)
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
+
+
 class Square:
     """
     Represents a square.
